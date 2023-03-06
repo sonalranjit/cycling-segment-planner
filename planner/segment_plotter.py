91f038218e79359fca0ffbc06dbe84ed48ef3fee
Sonal Ranjit <sonal.ranjit3@gmail.com>
2023-03-05 19:32:37 -0500

    added segment distance and elevation difference plotter

--- planner/segment_plotter.py
+++ planner/segment_plotter.py
@@ -3,10 +3,10 @@ import gpxpy.gpx
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.rcParams['axes.spines.top'] = False
-plt.rcParams['axes.spines.right'] = False
+plt.rcParams["axes.spines.top"] = False
+plt.rcParams["axes.spines.right"] = False
 
-with open('./data/rochester-to-eastern-divide.gpx', 'r') as gpx_file:
+with open("./data/rochester-to-eastern-divide.gpx", "r") as gpx_file:
     gpx = gpxpy.parse(gpx_file)
     print(f"Number of track points: {gpx.get_track_points_no()}")
     print(f"Elevation extremes: {gpx.get_elevation_extremes()}")
@@ -16,18 +16,20 @@ route_info = []
 for track in gpx.tracks:
     for segment in track.segments:
         for point in segment.points:
-            route_info.append({
-                'latitude': point.latitude,
-                'longitude': point.longitude,
-                'elevation': point.elevation
-            })
+            route_info.append(
+                {
+                    "latitude": point.latitude,
+                    "longitude": point.longitude,
+                    "elevation": point.elevation,
+                }
+            )
 print(route_info[:3])
 route_df = pd.DataFrame(route_info)
 print(route_df.head())
 
-route_df.to_csv('./data/rochester-to-eastern-divide.csv', index=False)
+route_df.to_csv("./data/rochester-to-eastern-divide.csv", index=False)
 
 plt.figure(figsize=(14, 8))
-plt.scatter(route_df['longitude'], route_df['latitude'], color='#101010')
-plt.title('Route latitude and longitude points', size=20)
+plt.scatter(route_df["longitude"], route_df["latitude"], color="#101010")
+plt.title("Route latitude and longitude points", size=20)
 plt.show()
---
+++ planner/segment_plotter_with_elevation_difference.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import haversine as hs
+
+plt.rcParams["figure.figsize"] = (16, 6)
+plt.rcParams["axes.spines.top"] = False
+plt.rcParams["axes.spines.right"] = False
+
+
+def haversine_distance(lat1, lon1, lat2, lon2) -> float:
+    distance = hs.haversine(
+        point1=(lat1, lon1), point2=(lat2, lon2), unit=hs.Unit.METERS
+    )
+    return np.round(distance, 2)
+
+
+route_df = pd.read_csv("./data/rochester-to-eastern-divide.csv")
+print(route_df)
+
+route_df["elevation_diff"] = route_df["elevation"].diff()
+print(route_df)
+
+distances = [np.nan]
+for i in range(len(route_df)):
+    if i == 0:
+        continue
+    else:
+        distances.append(
+            haversine_distance(
+                lat1=route_df.iloc[i - 1]["latitude"],
+                lon1=route_df.iloc[i - 1]["longitude"],
+                lat2=route_df.iloc[i]["latitude"],
+                lon2=route_df.iloc[i]["longitude"],
+            )
+        )
+
+route_df["distance"] = distances
+print(route_df.head())
+print(
+    f"Elevation difference: {route_df[route_df['elevation_diff'] >= 0]['elevation_diff'].sum()}"
+)
+print(f"Total distance: {route_df['distance'].sum()}")
+
+route_df["cum_elevation"] = route_df["elevation_diff"].cumsum()
+route_df["cum_distance"] = route_df["distance"].cumsum()
+print(route_df.head())
+
+route_df = route_df.fillna(0)
+print(route_df.head())
+route_df.to_csv("./data/rochester-to-eastern-divide-elevation-difference.csv")
+
+plt.plot(route_df["cum_distance"], route_df["cum_elevation"], color="#101010", lw=3)
+plt.title("Route elevation profile", size=20)
+plt.xlabel("Distance in meters", size=14)
+plt.ylabel("Elevation in meters", size=14)
+plt.show()
